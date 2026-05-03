37d7c1ebbdd231e766b63e52faf9771ab9c0561d
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-03 23:14:19 +0530

    chore: remove unused fs module import from app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const fs = require('fs');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
