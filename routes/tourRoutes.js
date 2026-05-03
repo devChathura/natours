53b5f8b8fff3fba60fb5848a502b823b0f0f6ccc
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-04 00:00:08 +0530

    refactor: add request body validation middleware to tourController.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,22 +11,13 @@
 const express = require('express');
 const router = express.Router();
 const tourController = require('../controllers/tourController');
-const checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};    
 
 router.param('id', tourController.checkID);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(checkBody, tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
