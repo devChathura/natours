f515bd1a7cd5f2efcff664ae6c63161b3461e683
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-04 12:42:29 +0530

    Configure dotenv for environment variables and add prod/dev scripts

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const morgan = require('morgan');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
