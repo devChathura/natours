d6ef0443b02ddd49e4ccc7d4fd22cbe918ad6954
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-04 15:25:31 +0530

    chore: update devDependencies and add ESLint configuration
    - Added ESLint and related plugins for code quality and style enforcement.
    - Updated nodemon version.
    - Minor formatting change in userRoutes.js for consistency.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,14 +1,5 @@
-// What we get when run: const tourController = require('./tourController');
-
-// {
-//   getAllTours: [Function (anonymous)],
-//   getTour: [Function (anonymous)],
-//   createTour: [Function (anonymous)],
-//   updateTour: [Function (anonymous)],
-//   deleteTour: [Function (anonymous)]
-// }
-
 const express = require('express');
+
 const router = express.Router();
 const tourController = require('../controllers/tourController');
 
