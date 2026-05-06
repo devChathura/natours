657535d441ddef4d71b14c12162d8ee577d2ea7b
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-06 10:33:42 +0530

    feat: add mongoose dependency and establish database connection in server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
+const DB_CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB_CONNECTION_STRING)
+  .then(() => console.log('DB connection successful!'));
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
