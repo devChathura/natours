fabe0701b2eb79d62a2a1d395c727304a396cc99
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-08 08:39:09 +0530

    feat: implement query filtering for getAllTours method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,13 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // QUERYING
+    const tours = await Tour.find(queryObj);
     res.status(200).json({
       status: 'success',
       results: tours.length,
