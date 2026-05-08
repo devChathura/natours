5c9b1ea78f042798dca95c777fc66937d460960a
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-08 13:55:28 +0530

    feat: enhance getAllTours method with sorting and field limiting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,12 +12,19 @@ exports.getAllTours = async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     let query = Tour.find(JSON.parse(queryStr));
-
+    // SORTING
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+    // FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     // QUERYING
