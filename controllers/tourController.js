62a00d0eefe8cca4d7fb5c93b6ec1ac4cde6ce5a
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-09 22:06:48 +0530

    feat: add aliasTopTours middleware for top 5 cheap tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('../models/tourModel');
 
+// MIDDLEWARE FOR THE ALIASING
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const tourController = require('../controllers/tourController');
 
 router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
@@ -14,3 +19,10 @@ router
   .delete(tourController.deleteTour);
 
 module.exports = router;
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next(); // Don't forget this!
+};
