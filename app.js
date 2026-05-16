ff5997f3ac931f233ea8f933785ee3a708562b8c
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-16 16:52:28 +0530

    refactor: extract error handling into dedicated controller and util class

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const morgan = require('morgan');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -14,19 +16,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.statusCode = 404;
-  err.status = 'fail';
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
