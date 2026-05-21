19b8604a7cd099133dca9da0beea2bc1aee46b11
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-21 11:12:10 +0530

    feat(server): gracefully handle unhandled promise rejections
    Added an event listener for 'unhandledRejection' in server.js.

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
