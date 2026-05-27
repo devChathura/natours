00e8b69733b8c9bdd3803dfcc967263699600710
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-27 11:00:14 +0530

    feat(auth): implement user login functionality
    - Add login controller with email/password verification
    - Add correctPassword instance method to userSchema using argon2
    - Register /login API route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const signToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, {
+  jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
@@ -41,4 +42,19 @@ exports.signup = catchAsync(async (req, res) => {
     passwordConfirm: req.body.passwordConfirm,
   });
   createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password!', 401));
+  }
+
+  createSendToken(user, 200, res);
 });
--- models/userModel.js
+++ models/userModel.js
@@ -45,6 +45,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await argon2.verify(userPassword, candidatePassword);
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
