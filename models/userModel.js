d19b3dbce58cbfd73a26209dbf68726b767a5f94
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-05-29 19:02:06 +0530

    feat: implement forgot password functionality
    - Added `nodemailer` dependency and `email.js` utility for sending emails
    - Created `forgotPassword` controller to generate reset tokens and send links
    - Updated User model to generate and store password reset tokens
    - Registered `/forgotPassword` and `/resetPassword/:token` routes

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const argon2 = require('argon2');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,6 +39,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function () {
@@ -67,6 +70,16 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   }
   // Return false if passwordChangedAt is not set
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
