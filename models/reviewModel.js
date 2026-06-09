b48d2c427009b685e41751903a68fc8b87479508
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-06-09 13:01:53 +0530

    feat(models): add review model with schema

---
+++ models/reviewModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const reviewModelSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review can not be empty'],
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: [true, 'A rating can not be empty'],
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'A review must belong to a tour'],
+      },
+    ],
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'A review must belong to a user'],
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+module.exports = mongoose.model('Review', reviewModelSchema);
