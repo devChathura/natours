21a2db3a75f7c46dc881da0a72bfd4cb431c8f4d
Chathura Hapukotuwa <hapukot-se22049@stu.kln.ac.lk>
2026-06-10 22:55:22 +0530

    chore(routes): clean up userRoutes.js for better readability and organization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,49 +7,68 @@ const authController = require('../controllers/authController');
 const validateRequest = require('../utils/validateRequest');
 const authValidation = require('../validations/authValidation');
 
+//api/v1/users/
+
+// Normal user sign up route
 router.post(
   '/signup',
   signupLimiter,
   validateRequest(authValidation.signupSchema),
   authController.signup,
 );
+
+// User login in route
 router.post(
   '/login',
   loginLimiter,
   validateRequest(authValidation.loginSchema),
   authController.login,
 );
+
+// User login in using OpenID connect(OAuth) route
 router.post(
   '/auth/google',
   loginLimiter,
   validateRequest(authValidation.googleLoginSchema),
   authController.googleLogin,
 );
+
+// Forget password Route
 router.post(
   '/forgotPassword',
   loginLimiter,
   validateRequest(authValidation.forgotPasswordSchema),
   authController.forgotPassword,
 );
+
+// Reset password Route
 router.patch(
   '/resetPassword/:token',
   validateRequest(authValidation.resetPasswordSchema),
   authController.resetPassword,
 );
+
+// Update Password Route
 router.patch(
   '/updateMyPassword',
   authController.protect,
   validateRequest(authValidation.updatePasswordSchema),
   authController.updatePassword,
 );
+
+// Update User
 router.patch(
   '/updateMe',
   authController.protect,
   validateRequest(authValidation.updateMeSchema),
   userController.updateMe,
 );
+
+// Delete User
 router.delete('/deleteMe', authController.protect, authController.deleteUser);
 
+// Routes for system Admins
+// TODO:apply authController.protect and authController.restrictTo('admin') middlewares
 router
   .route('/')
   .get(userController.getAllUsers)
